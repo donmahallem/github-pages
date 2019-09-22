c14c0b9812aa6f0b67ab499e29444327cb0dbac2
donmahallem <donmahallem@users.noreply.github.com>
2019-09-22 15:18:15 +0200

    bump

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+if [ ! -d "$INPUT_SOURCE" ]; then
+  echo "Source not defined"
+  exit 1
+fi
+if [ -z "$INPUT_TARGET" ]; then
+  echo "Target not defined"
+  exit 1
+fi
+
+echo "Deploying $INPUT_SOURCE directory to $INPUT_TARGET branch. (Force: $INPUT_FORCE_PUSH)"
