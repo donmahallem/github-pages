8c7777dfab2c48f576b5905e12fdd7f5102878fc
donmahallem <donmahallem@users.noreply.github.com>
2019-09-23 00:25:40 +0200

    Bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,6 +55,6 @@ else
 fi
 
 cd "$GITHUB_WORKSPACE"
-curl -v -XPOST -H "Authorization: token ${INPUT_DEPLOY_KEY}" -H "Accept: application/vnd.github.mister-fantastic-preview+json" https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds
+curl -XPOST -H "Authorization: token ${INPUT_DEPLOY_KEY}" -H "Accept: application/vnd.github.mister-fantastic-preview+json" https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds
 
 echo "Done"
