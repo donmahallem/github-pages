703f85a2a26f1f8f70ac64a1dfa4ed54798fd9eb
donmahallem <donmahallem@users.noreply.github.com>
2019-09-22 19:28:56 +0200

    bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,19 @@ fi
 echo "Deploying $INPUT_SOURCE directory to $INPUT_TARGET branch. (Force: $INPUT_FORCE_PUSH)"
 
 if [ "$INPUT_FORCE_PUSH" = true ]; then
-  echo "A"
+  echo "Commiting with force push"
+  cd "$INPUT_SOURCE"
+  git init
+  git config user.name "${GITHUB_ACTOR}"
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  git add *
+  git commit --allow-empty -m 'Github Pages Deployment' -m "Date ${date}"
+  git push --force --quiet https://"x-access-token:$GITHUB_TOKEN"@github.com/${GITHUB_REPOSITORY}.git master:${INPUT_TARGET}
+  rm -rf .git
+
+  cd "$GITHUB_WORKSPACE"
 else
   echo "B"
 fi
+
+echo "Done"
