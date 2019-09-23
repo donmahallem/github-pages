4b51675d0a784f6237ae45587cf1ff96ba04110a
donmahallem <donmahallem@users.noreply.github.com>
2019-09-23 10:01:35 +0200

    build

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,6 @@ if [ -z "$INPUT_TARGET" ]; then
   exit 1
 fi
 
-
-if [ -z "$INPUT_DEPLOY_KEY" ]; then
-  echo "Key not defined"
-  exit 1
-fi
-
 echo "Deploying $INPUT_SOURCE directory to $INPUT_TARGET branch. (Force: $INPUT_FORCE_PUSH)"
 
 if [ "$INPUT_FORCE_PUSH" = true ]; then
@@ -27,7 +21,7 @@ if [ "$INPUT_FORCE_PUSH" = true ]; then
   git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git add *
   git commit --allow-empty -m 'Github Pages Deployment' -m "Date $(date)"
-  git push --force --quiet https://"x-access-token:$GITHUB_TOKEN"@github.com/${GITHUB_REPOSITORY}.git master:${INPUT_TARGET}
+  git push --force --quiet https://"x-access-token:$INPUT_GITHUB_TOKEN"@github.com/${GITHUB_REPOSITORY}.git master:${INPUT_TARGET}
   rm -rf .git
 
 else
@@ -49,12 +43,21 @@ else
   cd "${GITHUB_WORKSPACE}/../gittemp"
   git add *
   git commit --allow-empty -m 'Github Pages Deployment' -m "Date $(date)"
-  git push --quiet https://"x-access-token:$GITHUB_TOKEN"@github.com/${GITHUB_REPOSITORY}.git ${INPUT_TARGET}:${INPUT_TARGET}
+  git push --quiet https://"x-access-token:$INPUT_GITHUB_TOKEN"@github.com/${GITHUB_REPOSITORY}.git ${INPUT_TARGET}:${INPUT_TARGET}
   cd "$GITHUB_WORKSPACE"
   rm -rf "${GITHUB_WORKSPACE}/../gittemp"
 fi
 
 cd "$GITHUB_WORKSPACE"
-curl -XPOST -H "Authorization: token ${INPUT_DEPLOY_KEY}" -H "Accept: application/vnd.github.mister-fantastic-preview+json" https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds
 
+
+if [ ! "$INPUT_PAT" = false ]; then
+  echo "Personal Access Token was provided. Queueing page build!"
+  curl -XPOST -H "Authorization: token ${INPUT_PAT}" -H "Accept: application/vnd.github.mister-fantastic-preview+json" https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds
+elif [ ! "$INPUT_DEPLOY_KEY" = false ]; then
+  echo "Deploy key was provided. Queueing page build!"
+  curl -XPOST -H "Authorization: token ${INPUT_DEPLOY_KEY}" -H "Accept: application/vnd.github.mister-fantastic-preview+json" https://api.github.com/repos/${GITHUB_REPOSITORY}/pages/builds
+else
+  echo "No key was provided. No page build queued!"
+fi
 echo "Done"
